8ee3455a44fc7ece47d4db8707c770b3e506edfa
IrynaSvitlychna <139795252+IrynaSvitlychna@users.noreply.github.com>
2024-04-08 18:20:46 +0300

    the end task baseTypes 1-9

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,14 +2,20 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
-let arrWorkingDays: string[] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
-
-
-function isWeekend(day: string): boolean {
-  for (const day of arrWorkingDays) {
-    if (arrWorkingDays.includes(day)) 
-      return { '${day} is working day'};
-  }
-  return { '${day} is weekend'}
+enum Weekday {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
 }
-isWeekend()
+
+
+function isWeekend(day: Weekday): boolean {
+  return (day === Weekday.SATURDAY || day === Weekday.SUNDAY) 
+}
+
+
+
