3f1781531140f5e4e10e27a8354835dbeffd94d0
IrynaSvitlychna <139795252+IrynaSvitlychna@users.noreply.github.com>
2024-04-10 23:19:30 +0300

    fixed error from mentor

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -2,11 +2,14 @@
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
 type Page = {
-  title: string;
-  likes: number;
-  accounts: string[];
+  title: string,
+  likes: number,
+  accounts: string[],
   status: 'open' | 'close',
-  details?: {};
+  details?: {
+    createAt: Date,
+    updateAt: Date,
+  };
 }
 
 const page1 = {
